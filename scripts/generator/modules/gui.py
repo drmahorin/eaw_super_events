35538711efe8077e7b1d910594d006e8c1da9708
mahorin <drmahorin@gmail.com>
2024-04-07 20:28:48 +0300

    Small fixes

--- scripts/generator/modules/gui.py
+++ scripts/generator/modules/gui.py
@@ -11,8 +11,6 @@ tcs = read_template('./templates/superevent_category_sg.txt')
 tsg = read_template('./templates/superevent_entry.txt')
 tss = read_template('./templates/superevent_entry_sg.txt')
 
-tsl = read_template('./templates/scripted_localisation.txt')
-
 ci = 0
 
 
@@ -23,7 +21,6 @@ def gui(out: str, cat: tuple[int, str], sub_ids: dict[str, dict[int, str]], c: i
     cat_cap = cat.upper()
     fg = open(f'{out}/interface/eawse_settings/eawse_{cat}.gui', 'w', encoding='utf-8')
     fs = open(f'{out}/common/scripted_guis/eawse_{cat}.txt', 'w', encoding='utf-8')
-    fl = open(f'{out}/common/scripted_localisation/eawse_sloc_c_{cat}.txt', 'w', encoding='utf-8')
 
     try:
         events = 0
@@ -41,7 +38,6 @@ def gui(out: str, cat: tuple[int, str], sub_ids: dict[str, dict[int, str]], c: i
             for i, id in ids.items():
                 fg.write(tsg(i=i, id=id, sub=sub_id, y=events*30))
                 fs.write(tss(cat=cat, i=i, id=id.lower()))
-                fl.write(tsl(id=id))
                 events += 1
                 if events < c:
                     fg.write('\n')
@@ -52,4 +48,3 @@ def gui(out: str, cat: tuple[int, str], sub_ids: dict[str, dict[int, str]], c: i
     finally:
         fg.close()
         fs.close()
-        fl.close()
